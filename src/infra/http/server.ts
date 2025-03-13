86353af40d7ed88365aea2bedfb7adf267b46c40
Ismael Borba <@cediacaonline.com>
2025-03-13 15:34:39 -0300

    validação e serialização com fastify e zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,34 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  //TODO: Enviar o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana)
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
