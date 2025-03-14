fc78809a4192d8851ef15fd9aa83c561f2060467
Ismael Borba <@cediacaonline.com>
2025-03-13 23:51:02 -0300

    adicionando teste no export uploads

--- src/app/use-cases/export-uploads.spec.ts
+++ src/app/use-cases/export-uploads.spec.ts
@@ -1,10 +1,21 @@
 import { randomUUID } from 'node:crypto'
+import { isRight, unwrapEither } from '@/infra/shared/either'
+import * as upload from '@/infra/storage/upload-file-to-storage'
 import { makeUpload } from '@/infra/test/factories/make-upload'
-import { describe, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { exportUploads } from './export-uploads'
 
 describe('Export uploads', () => {
   it('should be able to export uploads', async () => {
+    const uploadStub = vi
+      .spyOn(upload, 'uploadFileToStorage')
+      .mockImplementationOnce(async () => {
+        return {
+          key: `${randomUUID()}.csv`,
+          url: 'https://example.com/file.csv',
+        }
+      })
+
     const namePattern = randomUUID()
 
     const upload1 = await makeUpload({ name: `${namePattern}.webp` })
@@ -16,5 +27,41 @@ describe('Export uploads', () => {
     const sut = await exportUploads({
       searchQuery: namePattern,
     })
+
+    const generateCSVStream = uploadStub.mock.calls[0][0].contentStream
+
+    const csvAsString = await new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = []
+
+      generateCSVStream.on('data', chunk => {
+        chunks.push(chunk)
+      })
+
+      generateCSVStream.on('end', () => {
+        resolve(Buffer.concat(chunks).toString('utf-8'))
+      })
+
+      generateCSVStream.on('error', err => {
+        reject(err)
+      })
+    })
+
+    const csvAsArray = csvAsString
+      .trim()
+      .split('\n')
+      .map(row => row.split(','))
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut)).toEqual({
+      reportUrl: 'https://example.com/file.csv',
+    })
+    expect(csvAsArray).toEqual([
+      ['ID', 'Name', 'URL', 'Uploaded at'],
+      [upload1.id, upload1.name, upload1.remoteUrl, expect.any(String)],
+      [upload2.id, upload2.name, upload2.remoteUrl, expect.any(String)],
+      [upload3.id, upload3.name, upload3.remoteUrl, expect.any(String)],
+      [upload4.id, upload4.name, upload4.remoteUrl, expect.any(String)],
+      [upload5.id, upload5.name, upload5.remoteUrl, expect.any(String)],
+    ])
   })
 })
